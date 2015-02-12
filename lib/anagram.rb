425da51f9eb4fa6ddd8980520c90557dfe471e24
axton21 <axton_n@yahoo.com>
2015-02-12 13:04:06 -0500

    staring at the blank spaces baby

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+  require 'pry'
+
+  def initialize(string)
+    @string = string
+  end
+
+  def match(sentence)
+    @sentence = sentence
+    array = []
+    @sentence.each do |word|
+      sorted = word.split""
+      sorted.sort!
+      compare = @string.split""
+      compare.sort!
+      if compare == sorted
+         array <<word
+      end
+        # binding.pry
+    end
+    array
+  end
+end
